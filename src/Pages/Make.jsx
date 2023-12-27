f1259b655091a58ead3508aa44c3f1de06dd1043
leejieun <mariewldms2002@gmail.com>
2023-12-27 11:57:31 +0900

    최종

--- src/Pages/Make.jsx
+++ src/Pages/Make.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 export default function Make() {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [birthDate, setBirth] = useState("");
+  const [deathDate, setDeath] = useState("");
+  const [phoneNumber, setPhonenum] = useState("");
+  const [creater, setCreater] = useState("");
+  const [relationship, setRelations] = useState("");
+  const [postPeriod, setPost] = useState("");
+
+  const [file, setFile] = useState(null);
+  const [profileImageId, setFileId] = useState(null);
+
   const [inputs, setInputs] = useState({
     deathname: "",
     putname: "",
     relation: "",
     phone: "",
   });
-  const { deathname, putname, relation, phone } = inputs;
-  //=======================================================
+  const { putname, relation, phone } = inputs;
+
   const onChange = (e) => {
     const { name, value } = e.target;
     setInputs((prevInputs) => ({
@@ -21,112 +31,141 @@ export default function Make() {
       [name]: value,
     }));
   };
-  //=====================================================
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("분향소가 등록되었습니다");
-    navigate(`/`);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
   };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await axios.post("/v1/image-upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (response.status === 200) {
+        const result = response.data;
+        console.log("파일 ID:", result.file_id);
+        setFileId(result.file_id);
+      } else {
+        console.error("파일 업로드 실패");
+      }
+    } catch (error) {
+      console.error("파일 업로드 중 오류 발생:", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await handleUpload();
+
+      const data = {
+        name,
+        phoneNumber,
+        creator: creater,
+        birthDate,
+        deathDate,
+        postPeriod: "month",
+        relationship,
+        profileImageId,
+      };
+
+      try {
+        const response = await axios.post(
+          "/reborner",
+          JSON.stringify(data), // 데이터를 JSON 문자열로 변환
+          {
+            headers: {
+              "Content-Type": "application/json", // JSON 데이터를 보낼 때 Content-Type을 application/json으로 설정
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          alert("분향소가 등록되었습니다");
+          navigate(`/`);
+        } else {
+          console.error("데이터 제출 실패");
+        }
+      } catch (error) {
+        console.log("fail", error);
+      }
+    } catch (error) {
+      console.error("데이터 제출 중 오류 발생:", error);
+    }
+  };
+
   return (
-    <MakeDeath flex>
+    <MakeDeath>
       <div className="middle">
-        <div className="make1">스페이스 생성</div>
+        <div className="make1">분향소생성</div>
       </div>
 
-      <br></br>
+      <br />
       <form onSubmit={handleSubmit}>
-        <div>
-          <hr></hr>
-        </div>
-        <div className="middle">
-          <div className="make">RE:BORNER정보</div>
-        </div>
-        <br></br>
-        <br></br>
         <div className="checkinfo1">
-          <div>RE:BORNER명</div>
-          <input
-            type="text"
-            value={deathname}
-            name="deathname"
-            onChange={onChange}
-          ></input>
-
+          <div>고인명</div>
+          <input type="text" onChange={(e) => setName(e.target.value)} />
           <div>사진 업로드</div>
-          <input type="button" value="업로드"></input>
+          <input type="file" onChange={handleFileChange} />
+          <input type="button" value="업로드" onClick={handleUpload} />
           <div>사망확인서 업로드</div>
-          <input type="button" value="업로드"></input>
+          <input type="button" value="업로드" />
         </div>
-        <br></br>
+
         <div className="checkinfo1">
           <div>생년월일</div>
-          <input type="date"></input>
+          <input type="date" onChange={(e) => setBirth(e.target.value)} />
           <div>발인날짜</div>
-          <input type="date"></input>
+          <input type="date" onChange={(e) => setDeath(e.target.value)} />
         </div>
-        <br></br>
-        <div>
-          <hr></hr>
-        </div>
-        <div className="middle">
-          <div className="make">등록자정보</div>
-        </div>
-        <br></br>
-        <br></br>
+
         <div className="checkinfo1">
           <div>등록자명</div>
-          <input
-            type="text"
-            value={putname}
-            name="putname"
-            onChange={onChange}
-          ></input>
+          <input type="text" onChange={(e) => setCreater(e.target.value)} />
           <div>가족관계증명서 업로드</div>
-          <input type="button" value="업로드"></input>
+          <input type="button" value="업로드" />
         </div>
-        <br></br>
+
         <div className="checkinfo1">
-          <div>RE:BORNER와의 관계</div>
-          <input
-            type="text"
-            value={relation}
-            name="relation"
-            onChange={onChange}
-          ></input>
+          <div>고인과의 관계</div>
+          <input type="text" onChange={(e) => setRelations(e.target.value)} />
           <div>전화번호</div>
-          <input
-            type="text"
-            value={phone}
-            name="phone"
-            onChange={onChange}
-          ></input>
+          <input type="text" onChange={(e) => setPhonenum(e.target.value)} />
         </div>
-        <br></br>
+
         <div className="checkinfo1">
-          <div>온라인 스페이스 개방 기간</div>
-          <div class="radio-group">
+          <div>온라인 분향소 개방 기간</div>
+          <div
+            className="radio-group"
+            onChange={(e) => setPost(e.target.value)}
+          >
             <label>
-              <input type="radio" name="option" />
+              <input type="radio" name="option" value="week" />
               7일
             </label>
             <label>
-              <input type="radio" name="option" />한 달
+              <input type="radio" name="option" value="month" />한 달
             </label>
             <label>
-              <input type="radio" name="option" />
+              <input type="radio" name="option" value="year" />
               1년
             </label>
             <label>
-              <input type="radio" name="option" />
+              <input type="radio" name="option" value="indefinite" />
               무기한
             </label>
           </div>
         </div>
-        <br></br>
-        <br></br>
-        <br></br>
+
         <div className="middle">
-          <Submit type="submit" disabled={loading} />
+          <Submit type="submit" />
         </div>
       </form>
     </MakeDeath>
